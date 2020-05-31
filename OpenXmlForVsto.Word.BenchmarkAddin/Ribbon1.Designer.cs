4270be6050553eeae74d2eeca56e0a007641b553
Alex Butenko <butenko.alexey@gmail.com>
2020-05-31 09:25:14 +0800

    Word: added an example project
    This porject shows how to use this library.
    Also it is a benchmark, and it can measure how long operations take by
    using interop and OpenXML.

---
+++ OpenXmlForVsto.Word.BenchmarkAddin/Ribbon1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace OpenXmlForVsto.Word.BenchmarkAddin {
+    partial class Ribbon1 : Microsoft.Office.Tools.Ribbon.RibbonBase {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public Ribbon1()
+            : base(Globals.Factory.GetRibbonFactory()) {
+            InitializeComponent();
+        }
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.BenchmarkAddinTab = this.Factory.CreateRibbonTab();
+            this.GroupVsto = this.Factory.CreateRibbonGroup();
+            this.ButtonWrite1RunVsto = this.Factory.CreateRibbonButton();
+            this.ButtonWrite100RunsVsto = this.Factory.CreateRibbonButton();
+            this.ButtonWrite10kRunsVsto = this.Factory.CreateRibbonButton();
+            this.ButtonWrite100kRunsVsto = this.Factory.CreateRibbonButton();
+            this.separator1 = this.Factory.CreateRibbonSeparator();
+            this.ButtonReadVsto = this.Factory.CreateRibbonButton();
+            this.GroupOpenXML = this.Factory.CreateRibbonGroup();
+            this.ButtonWrite1RunOpenXML = this.Factory.CreateRibbonButton();
+            this.ButtonWrite100RunsOpenXML = this.Factory.CreateRibbonButton();
+            this.ButtonWrite10kRunsOpenXML = this.Factory.CreateRibbonButton();
+            this.ButtonWrite100kRunsOpenXML = this.Factory.CreateRibbonButton();
+            this.ButtonWrite1mRunsOpenXML = this.Factory.CreateRibbonButton();
+            this.separator3 = this.Factory.CreateRibbonSeparator();
+            this.ButtonReadOpenXML = this.Factory.CreateRibbonButton();
+            this.BenchmarkAddinTab.SuspendLayout();
+            this.GroupVsto.SuspendLayout();
+            this.GroupOpenXML.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // BenchmarkAddinTab
+            // 
+            this.BenchmarkAddinTab.Groups.Add(this.GroupVsto);
+            this.BenchmarkAddinTab.Groups.Add(this.GroupOpenXML);
+            this.BenchmarkAddinTab.Label = "Benchmark";
+            this.BenchmarkAddinTab.Name = "BenchmarkAddinTab";
+            // 
+            // GroupVsto
+            // 
+            this.GroupVsto.Items.Add(this.ButtonWrite1RunVsto);
+            this.GroupVsto.Items.Add(this.ButtonWrite100RunsVsto);
+            this.GroupVsto.Items.Add(this.ButtonWrite10kRunsVsto);
+            this.GroupVsto.Items.Add(this.ButtonWrite100kRunsVsto);
+            this.GroupVsto.Items.Add(this.separator1);
+            this.GroupVsto.Items.Add(this.ButtonReadVsto);
+            this.GroupVsto.Label = "VSTO";
+            this.GroupVsto.Name = "GroupVsto";
+            // 
+            // ButtonWrite1RunVsto
+            // 
+            this.ButtonWrite1RunVsto.Label = "Write 1 run";
+            this.ButtonWrite1RunVsto.Name = "ButtonWrite1RunVsto";
+            this.ButtonWrite1RunVsto.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.ButtonWrite1RunVsto_Click);
+            // 
+            // ButtonWrite100RunsVsto
+            // 
+            this.ButtonWrite100RunsVsto.Label = "Write 100 runs";
+            this.ButtonWrite100RunsVsto.Name = "ButtonWrite100RunsVsto";
+            this.ButtonWrite100RunsVsto.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.ButtonWrite100RunsVsto_Click);
+            // 
+            // ButtonWrite10kRunsVsto
+            // 
+            this.ButtonWrite10kRunsVsto.Label = "Write 10k runs";
+            this.ButtonWrite10kRunsVsto.Name = "ButtonWrite10kRunsVsto";
+            this.ButtonWrite10kRunsVsto.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.ButtonWrite10kRunsVsto_Click);
+            // 
+            // ButtonWrite100kRunsVsto
+            // 
+            this.ButtonWrite100kRunsVsto.Label = "Write 100k runs";
+            this.ButtonWrite100kRunsVsto.Name = "ButtonWrite100kRunsVsto";
+            this.ButtonWrite100kRunsVsto.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.ButtonWrite100kRunsVsto_Click);
+            // 
+            // separator1
+            // 
+            this.separator1.Name = "separator1";
+            // 
+            // ButtonReadVsto
+            // 
+            this.ButtonReadVsto.Label = "Read";
+            this.ButtonReadVsto.Name = "ButtonReadVsto";
+            this.ButtonReadVsto.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.ButtonReadVsto_Click);
+            // 
+            // GroupOpenXML
+            // 
+            this.GroupOpenXML.Items.Add(this.ButtonWrite1RunOpenXML);
+            this.GroupOpenXML.Items.Add(this.ButtonWrite100RunsOpenXML);
+            this.GroupOpenXML.Items.Add(this.ButtonWrite10kRunsOpenXML);
+            this.GroupOpenXML.Items.Add(this.ButtonWrite100kRunsOpenXML);
+            this.GroupOpenXML.Items.Add(this.ButtonWrite1mRunsOpenXML);
+            this.GroupOpenXML.Items.Add(this.separator3);
+            this.GroupOpenXML.Items.Add(this.ButtonReadOpenXML);
+            this.GroupOpenXML.Label = "OpenXML";
+            this.GroupOpenXML.Name = "GroupOpenXML";
+            // 
+            // ButtonWrite1RunOpenXML
+            // 
+            this.ButtonWrite1RunOpenXML.Label = "Write 1 run";
+            this.ButtonWrite1RunOpenXML.Name = "ButtonWrite1RunOpenXML";
+            this.ButtonWrite1RunOpenXML.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.ButtonWrite1RunOpenXML_Click);
+            // 
+            // ButtonWrite100RunsOpenXML
+            // 
+            this.ButtonWrite100RunsOpenXML.Label = "Write 100 runs";
+            this.ButtonWrite100RunsOpenXML.Name = "ButtonWrite100RunsOpenXML";
+            this.ButtonWrite100RunsOpenXML.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.ButtonWrite100RunsOpenXML_Click);
+            // 
+            // ButtonWrite10kRunsOpenXML
+            // 
+            this.ButtonWrite10kRunsOpenXML.Label = "Write 10k runs";
+            this.ButtonWrite10kRunsOpenXML.Name = "ButtonWrite10kRunsOpenXML";
+            this.ButtonWrite10kRunsOpenXML.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.ButtonWrite10kRunsOpenXML_Click);
+            // 
+            // ButtonWrite100kRunsOpenXML
+            // 
+            this.ButtonWrite100kRunsOpenXML.Label = "Write 100k runs";
+            this.ButtonWrite100kRunsOpenXML.Name = "ButtonWrite100kRunsOpenXML";
+            this.ButtonWrite100kRunsOpenXML.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.ButtonWrite100kRunsOpenXML_Click);
+            // 
+            // ButtonWrite1mRunsOpenXML
+            // 
+            this.ButtonWrite1mRunsOpenXML.Label = "Write 1m runs";
+            this.ButtonWrite1mRunsOpenXML.Name = "ButtonWrite1mRunsOpenXML";
+            this.ButtonWrite1mRunsOpenXML.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.ButtonWrite1mRunsOpenXML_Click);
+            // 
+            // separator3
+            // 
+            this.separator3.Name = "separator3";
+            // 
+            // ButtonReadOpenXML
+            // 
+            this.ButtonReadOpenXML.Label = "Read";
+            this.ButtonReadOpenXML.Name = "ButtonReadOpenXML";
+            this.ButtonReadOpenXML.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.ButtonReadOpenXML_Click);
+            // 
+            // Ribbon1
+            // 
+            this.Name = "Ribbon1";
+            this.RibbonType = "Microsoft.Word.Document";
+            this.Tabs.Add(this.BenchmarkAddinTab);
+            this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.Ribbon1_Load);
+            this.BenchmarkAddinTab.ResumeLayout(false);
+            this.BenchmarkAddinTab.PerformLayout();
+            this.GroupVsto.ResumeLayout(false);
+            this.GroupVsto.PerformLayout();
+            this.GroupOpenXML.ResumeLayout(false);
+            this.GroupOpenXML.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal Microsoft.Office.Tools.Ribbon.RibbonTab BenchmarkAddinTab;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup GroupVsto;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton ButtonReadVsto;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton ButtonWrite1RunVsto;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton ButtonWrite100RunsVsto;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton ButtonWrite10kRunsVsto;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton ButtonWrite100kRunsVsto;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup GroupOpenXML;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton ButtonReadOpenXML;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton ButtonWrite1RunOpenXML;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton ButtonWrite100RunsOpenXML;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton ButtonWrite10kRunsOpenXML;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton ButtonWrite100kRunsOpenXML;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton ButtonWrite1mRunsOpenXML;
+        internal Microsoft.Office.Tools.Ribbon.RibbonSeparator separator1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonSeparator separator3;
+    }
+
+    partial class ThisRibbonCollection {
+        internal Ribbon1 Ribbon1 {
+            get {
+                return this.GetRibbon<Ribbon1>();
+            }
+        }
+    }
+}
---
+++ OpenXmlForVsto.Word.BenchmarkAddin/Ribbon1.cs
@@ -0,0 +1,191 @@
+﻿using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using Microsoft.Office.Tools.Ribbon;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Xceed.Document.NET;
+using Xceed.Words.NET;
+using Interop = Microsoft.Office.Interop.Word;
+using OpenXml = DocumentFormat.OpenXml.Wordprocessing;
+
+namespace OpenXmlForVsto.Word.BenchmarkAddin {
+    public partial class Ribbon1 {
+        void Ribbon1_Load(object sender, RibbonUIEventArgs e) { }
+
+        void ButtonWrite1RunVsto_Click(object sender, RibbonControlEventArgs e) => WriteVsto(1);
+
+        void ButtonWrite100RunsVsto_Click(object sender, RibbonControlEventArgs e) => WriteVsto(10);
+
+        void ButtonWrite10kRunsVsto_Click(object sender, RibbonControlEventArgs e) => WriteVsto(100);
+
+        void ButtonWrite100kRunsVsto_Click(object sender, RibbonControlEventArgs e) => WriteVsto(316);
+
+        void ButtonReadVsto_Click(object sender, RibbonControlEventArgs e) => ReadVsto();
+
+        async void ButtonWrite1RunOpenXML_Click(object sender, RibbonControlEventArgs e) => await WriteOpenXml(1);
+
+        async void ButtonWrite100RunsOpenXML_Click(object sender, RibbonControlEventArgs e) => await WriteOpenXml(10);
+
+        async void ButtonWrite10kRunsOpenXML_Click(object sender, RibbonControlEventArgs e) => await WriteOpenXml(100);
+
+        async void ButtonWrite1mRunsOpenXML_Click(object sender, RibbonControlEventArgs e) => await WriteOpenXml(1000);
+
+        async void ButtonWrite100kRunsOpenXML_Click(object sender, RibbonControlEventArgs e) => await WriteOpenXml(316);
+
+        async void ButtonReadOpenXML_Click(object sender, RibbonControlEventArgs e) => await ReadOpenXml();
+
+        Interop.Application _app => Globals.ThisAddIn.Application;
+        readonly Random _rand = new Random();
+
+        void WriteVsto(int size) {
+            Stopwatch sw = Stopwatch.StartNew();
+
+            _app.ScreenUpdating = false;
+
+            Interop.WdColor[] colors = {
+                Interop.WdColor.wdColorRed,
+                Interop.WdColor.wdColorGreen,
+                Interop.WdColor.wdColorBlue,
+                Interop.WdColor.wdColorGold,
+                Interop.WdColor.wdColorAqua,
+                Interop.WdColor.wdColorPink,
+                Interop.WdColor.wdColorDarkBlue };
+            Interop.Document document = _app.ActiveDocument;
+            for (int i = 1; i <= size; i++) {
+                Interop.Paragraph paragraph = document.Range().Paragraphs.Add();
+                for (int j = 1; j <= size; j++) {
+                    Interop.Range range = paragraph.Range;
+                    range.Collapse(Interop.WdCollapseDirection.wdCollapseEnd);
+                    range.Text = " " + (1000 * i + j);
+                    Interop.Font font = range.Font;
+                    font.Fill.BackColor.RGB = GenerateRandomColor();
+                    font.Color = colors[_rand.Next(6)];
+                    font.Size = _rand.Next(9, 14);
+                    font.Italic = -_rand.Next(2);
+                    font.Bold = -_rand.Next(2);
+                }
+            }
+
+            _app.ScreenUpdating = true;
+
+            sw.Stop();
+            MessageBox.Show(sw.Elapsed.ToString(), $"VSTO: write {size * size}");
+        }
+
+        void ReadVsto() {
+            Stopwatch sw = Stopwatch.StartNew();
+
+            _app.ScreenUpdating = false;
+
+            Interop.Document document = _app.ActiveDocument;
+            int i = 0;
+            foreach (Interop.Range range in document.Range().Words) {
+                string a1 = range.Text;
+                Interop.Font font = range.Font;
+                int a2 = font.Fill.BackColor.RGB;
+                Interop.WdColor a3 = font.Color;
+                float a4 = font.Size;
+                int a5 = font.Italic;
+                int a6 = font.Bold;
+                i++;
+            }
+
+            _app.ScreenUpdating = true;
+
+            sw.Stop();
+            MessageBox.Show(sw.Elapsed.ToString(), $"VSTO: read {i}");
+        }
+
+        async Task WriteOpenXml(int size) {
+            Stopwatch sw = Stopwatch.StartNew();
+
+            _app.ScreenUpdating = false;
+
+            Interop.Range range = _app.ActiveDocument.Range();
+            string tmpFile = new OpenXmlHelper().CopyToFile(range);
+
+            _app.ScreenUpdating = true;
+
+            await Task.Run(() => {
+                Color[] colors = { Color.Red, Color.Green, Color.Blue, Color.Gold, Color.Gray, Color.Pink, Color.GreenYellow };
+                using (DocX doc = DocX.Load(tmpFile)) {
+                    for (int i = 1; i <= size; i++) {
+                        Paragraph paragraph = doc.InsertParagraph();
+                        for (int j = 1; j <= size; j++) {
+                            Paragraph p = paragraph.Append(" " + 1000 * i + j)
+                                .Shading(Color.FromArgb(GenerateRandomColor()))
+                                .Color(colors[_rand.Next(6)])
+                                .FontSize(_rand.Next(9, 14));
+                            if (_rand.Next(2) == 1) {
+                                p.Bold();
+                            }
+                            if (_rand.Next(2) == 1) {
+                                p.Italic();
+                            }
+                        }
+                    }
+
+                    doc.Save();
+                }
+            });
+
+            range.Text = " ";
+            _app.ScreenUpdating = false;
+            new OpenXmlHelper().CopyFromFile(tmpFile, range);
+            _app.ScreenUpdating = true;
+
+            File.Delete(tmpFile);
+
+            sw.Stop();
+            MessageBox.Show(sw.Elapsed.ToString(), $"OpenXML: write {size * size}");
+        }
+
+        async Task ReadOpenXml() {
+            Stopwatch sw = Stopwatch.StartNew();
+
+            _app.ScreenUpdating = false;
+
+            Interop.Range range = _app.ActiveDocument.Range();
+
+            string tmpFile = new OpenXmlHelper().CopyToFile(range);
+
+            _app.ScreenUpdating = true;
+
+            int i = 0;
+            await Task.Run(() => {
+                using (WordprocessingDocument doc = WordprocessingDocument.Open(tmpFile, true)) {
+                    IEnumerable<OpenXml.Paragraph> paragraphs = doc.MainDocumentPart
+                        .Document
+                        .Body
+                        .Elements<OpenXml.Paragraph>();
+                    foreach (OpenXml.Paragraph paragraph in paragraphs) {
+                        foreach (OpenXml.Run run in paragraph.Elements<OpenXml.Run>()) {
+                            OpenXml.RunProperties properties = run.RunProperties;
+                            string text = run.Elements<OpenXml.Text>().FirstOrDefault()?.Text;
+                            OpenXml.Bold bold = properties?.Bold;
+                            OpenXml.Italic italic = properties?.Italic;
+                            OpenXml.Color color = properties?.Color;
+                            StringValue backColor = properties?.Shading.Fill;
+                            OpenXml.FontSize fontSize = properties?.FontSize;
+                            i++;
+                        }
+                    }
+                }
+            });
+
+            File.Delete(tmpFile);
+
+            sw.Stop();
+            MessageBox.Show(sw.Elapsed.ToString(), $"OpenXML: read {i}");
+        }
+
+        int GenerateRandomColor() =>
+            _rand.Next(255) * 256 * 256 + _rand.Next(255) * 256 + _rand.Next(255);
+    }
+}
